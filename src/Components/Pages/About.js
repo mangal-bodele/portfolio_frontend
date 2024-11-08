735cf62bd007a147486dcee9732f8e759d898613
mangal-bodele <163996259+mangal-bodele@users.noreply.github.com>
2024-11-08 12:42:40 +0530

    Update About.js

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
 
 function About() {
+  const skills = [
+    { src: './Static/python.png', alt: 'Python' },
+    { src: './Static/django.png', alt: 'Django' },
+    { src: './Static/restframework.png', alt: 'Rest Framework' },
+    { src: './Static/mysql.png', alt: 'MySQL' },
+    { src: './Static/javascript.png', alt: 'JavaScript' },
+    { src: './Static/css.png', alt: 'CSS' },
+    { src: './Static/html.png', alt: 'HTML' },
+    { src: './Static/react.png', alt: 'React' },
+    { src: './Static/bootstrap.png', alt: 'Bootstrap' },
+    { src: './Static/github.png', alt: 'GitHub' },
+  ];
+
   return (
-    <div className="container about-section mt-5 p-5" style={{ maxWidth: '1200px', marginTop: '40px' }}>
-      <div className="row">
+    <div className="container about-section mt-5 p-5" style={{ maxWidth: '1200px' }}>
+      <section aria-label="Skills" className="row mb-5">
         <div className="col-md-6">
           <h2 className="text-center mb-4">Skills</h2>
           <ul className="list-unstyled d-flex flex-wrap justify-content-md-start justify-content-center">
-            {[
-              { src: './Static/python.png', alt: 'Python' },
-              { src: './Static/django.png', alt: 'Django' },
-              { src: './Static/restframework.png', alt: 'Rest Framework' },
-              { src: './Static/mysql.png', alt: 'MySQL' },
-              { src: './Static/javascript.png', alt: 'JavaScript' },
-              { src: './Static/css.png', alt: 'CSS' },
-              { src: './Static/html.png', alt: 'HTML' },
-              { src: './Static/react.png', alt: 'React' },
-              { src: './Static/bootstrap.png', alt: 'Bootstrap' },
-              { src: './Static/github.png', alt: 'GitHub' },
-            ].map((skill, index) => (
+            {skills.map((skill, index) => (
               <li key={index} className="m-3">
                 <img
                   src={skill.src}
@@ -31,7 +32,9 @@ function About() {
             ))}
           </ul>
         </div>
+      </section>
 
+      <section aria-label="About Me" className="row">
         <div className="col-md-6">
           <h2 className="text-center mb-4">About Me</h2>
           <p>
@@ -40,17 +43,17 @@ function About() {
           <p>
             My skills include JavaScript, React, Python, Django, and Bootstrap. I'm constantly learning and staying up-to-date with the latest tech trends.
           </p>
-          <Link 
-            to="https://wa.me/919284662053" 
+          <a 
+            href="https://wa.me/919284662053" 
             className="btn btn-success btn-lg mt-3"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="Contact Me via WhatsApp"
           >
             Contact Me
-          </Link>
+          </a>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
