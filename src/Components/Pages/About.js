c946ff58be420d627cfa13e3f3aebdcd64bb59da
mangal-bodele <163996259+mangal-bodele@users.noreply.github.com>
2024-11-08 12:32:37 +0530

    Update About.js

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -8,86 +8,27 @@ function About() {
         <div className="col-md-6">
           <h2 className="text-center mb-4">Skills</h2>
           <ul className="list-unstyled d-flex flex-wrap justify-content-md-start justify-content-center">
-            <li className="m-3">
-              <img
-                src='./Static/python.png'
-                alt="Python"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/django.png'
-                alt="Django"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/restframework.png'
-                alt="Rest Framework"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/mysql.png'
-                alt="MySQL"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/javascript.png'
-                alt="JavaScript"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/css.png'
-                alt="CSS"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/html.png'
-                alt="HTML"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/react.png'
-                alt="React"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/bootstrap.png'
-                alt="Bootstrap"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
-            <li className="m-3">
-              <img
-                src='./Static/github.png'
-                alt="GitHub"
-                className="img-fluid shadow-sm"
-                style={{ width: '70px', height: '70px', objectFit: 'contain' }}
-              />
-            </li>
+            {[
+              { src: './Static/python.png', alt: 'Python' },
+              { src: './Static/django.png', alt: 'Django' },
+              { src: './Static/restframework.png', alt: 'Rest Framework' },
+              { src: './Static/mysql.png', alt: 'MySQL' },
+              { src: './Static/javascript.png', alt: 'JavaScript' },
+              { src: './Static/css.png', alt: 'CSS' },
+              { src: './Static/html.png', alt: 'HTML' },
+              { src: './Static/react.png', alt: 'React' },
+              { src: './Static/bootstrap.png', alt: 'Bootstrap' },
+              { src: './Static/github.png', alt: 'GitHub' },
+            ].map((skill, index) => (
+              <li key={index} className="m-3">
+                <img
+                  src={skill.src}
+                  alt={skill.alt}
+                  className="img-fluid shadow-sm"
+                  style={{ width: '70px', height: '70px', objectFit: 'contain' }}
+                />
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -99,7 +40,15 @@ function About() {
           <p>
             My skills include JavaScript, React, Python, Django, and Bootstrap. I'm constantly learning and staying up-to-date with the latest tech trends.
           </p>
-          <Link to="/#contacts" className="btn btn-success btn-lg mt-3">Contact Me</Link>
+          <Link 
+            to="https://wa.me/919284662053" 
+            className="btn btn-success btn-lg mt-3"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Contact Me via WhatsApp"
+          >
+            Contact Me
+          </Link>
         </div>
       </div>
     </div>
